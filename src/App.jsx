790823ec85aa8305e89fe64b75b9fcc8fdfe78cc
Hanu <hanugupta.knp2019@gmail.com>
2024-03-09 16:48:25 +0530

    created pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Profile from './pages/Profile';
 
 const App = () => {
   return (
-    <div className='text-red-500'>
-      Hello!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={  <SignIn />} />
+        <Route path="/sign-up" element={  <SignUp />} />
+        <Route path="/profile" element={  <Profile />} />
+        <Route path="/about" element={  <About />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
