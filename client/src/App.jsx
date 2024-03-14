0797fb2c16f0a40632003acf6b758ad5093a1532
Hanu <hanugupta.knp2019@gmail.com>
2024-03-14 19:06:02 +0530

    update and make profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import SignUp from './pages/SignUp';
-import SignIn from './pages/SignIn';
-import Profile from './pages/Profile';
-import Header from './components/Header';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={  <SignIn />} />
-        <Route path="/sign-up" element={  <SignUp />} />
-        <Route path="/profile" element={  <Profile />} />
-        <Route path="/about" element={  <About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -15,7 +15,7 @@ const userSlice = createSlice({
         },
         signInSuccess: (state, action) => {
             state.loading = false;
-            state.user = action.payload;
+            state.currentUser = action.payload;
             state.error=null;
         },
         signInFailure: (state, action) => {
